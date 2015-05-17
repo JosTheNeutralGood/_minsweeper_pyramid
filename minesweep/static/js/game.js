efd4bf067717039727cb0c04a68e3c6a5f85a8c3
Lana <lana.lodge@gmail.com>
2015-05-17 18:35:06 -0400

    board construction debugging and o'clock functions for buttons.

--- minesweep/static/js/game.js
+++ minesweep/static/js/game.js
@@ -6,9 +6,18 @@ function start_game(start_conditions){
 	var total_bombs = start_conditions['b'];
 	var b_height = start_conditions['h'];
 	var b_length = start_conditions['l'];
-	var board = init_board;
-}
-
+	var board = init_board(b_length, b_height, total_bombs);
+	var reset_clock = initialize_clock;
+	$(".board-unit").click( function(e){ click_unit($(e.target)); } );
+	
+	function click_unit(unit){
+		var params = unit.attr('id').split("-");
+		var y = parseInt(params[0]);
+		var x = parseInt(params[1]);
+		console.log("x" + x.toString() + " y" + y.toString());
+		console.log(board[x][y]);
+	};
+};
 
 function init_board(w, h, m) {
   var board = blank_board(w, h);
@@ -19,73 +28,75 @@ function init_board(w, h, m) {
   var mine_right;
   for(var i =0;i<m;i++) {
     mine = unique_mine(board, w, h);
-    board[mine[x]][mine[y]] = 'm'
-    mine_top = (mine[y] == 0)
-    mine_left = (mine[x] == 0)
-    mine_right = (mine[x] == w-1)
-    mine_bottom = (mine[y] == h-1)
+    board[mine['x']][mine['y']] = 'm';
+    mine_top = (mine['y'] == 0);
+    mine_left = (mine['x'] == 0);
+    mine_right = (mine['x'] == w-1);
+    mine_bottom = (mine['y'] == h-1);
     //incrementing upper left
-    if(!mine_top and !mine_left and board[mine[x]-1][mine[y]-1] != 'm'){
-      board[mine[x]-1][mine[y]-1] = board[mine[x]-1][mine[y]-1] + 1;
+    if(!mine_top && !mine_left && board[mine['x']-1][mine['y']-1] != 'm'){
+      board[mine['x']-1][mine['y']-1] = board[mine['x']-1][mine['y']-1] + 1;
     }
     //incrementing above
-    if(!mine_top and board[mine[x]][mine[y]-1] != 'm'){
-      board[mine[x]][mine[y]-1] = board[mine[x]][mine[y]-1] + 1;
+    if(!mine_top && board[mine['x']][mine['y']-1] != 'm'){
+      board[mine['x']][mine['y']-1] = board[mine['x']][mine['y']-1] + 1;
     }
     //incrementing upper right
-    if(!mine_top and !mine_right and board[mine[x]+1][mine[y]-1] != 'm'){
-      board[mine[x]+1][mine[y]-1] = board[mine[x]+1][mine[y]-1] + 1;
+    if(!mine_top && !mine_right && board[mine['x']+1][mine['y']-1] != 'm'){
+      board[mine['x']+1][mine['y']-1] = board[mine['x']+1][mine['y']-1] + 1;
     }
     //incrementing right
-    if(!mine_right and board[mine[x]+1][mine[y]] != 'm'){
-      board[mine[x]+1][mine[y]] = board[mine[x]+1][mine[y]] + 1;
+    if(!mine_right && board[mine['x']+1][mine['y']] != 'm'){
+      board[mine['x']+1][mine['y']] = board[mine['x']+1][mine['y']] + 1;
     }
     //incrementing bottom right
-    if(!mine_bottom and !mine_right and board[mine[x]+1][mine[y]+1] != 'm'){
-      board[mine[x]+1][mine[y]+1] = board[mine[x]+1][mine[y]+1] + 1;
+    if(!mine_bottom && !mine_right && board[mine['x']+1][mine['y']+1] != 'm'){
+      board[mine['x']+1][mine['y']+1] = board[mine['x']+1][mine['y']+1] + 1;
     }
     //incrementing bellow
-    if(!mine_bottom and board[mine[x]][mine[y]+1] != 'm'){
-      board[mine[x]][mine[y]+1] = board[mine[x]][mine[y]+1] + 1;
+    if(!mine_bottom && board[mine['x']][mine['y']+1] != 'm'){
+      board[mine['x']][mine['y']+1] = board[mine['x']][mine['y']+1] + 1;
     }
      //incrementing bottom left
-    if(!mine_bottom and !mine_left and board[mine[x]-1][mine[y]+1] != 'm'){
-      board[mine[x]-1][mine[y]+1] = board[mine[x]-1][mine[y]+1] + 1;
+    if(!mine_bottom && !mine_left && board[mine['x']-1][mine['y']+1] != 'm'){
+      board[mine['x']-1][mine['y']+1] = board[mine['x']-1][mine['y']+1] + 1;
     }
     //incrementing left
-    if(!mine_left and board[mine[x]-1][mine[y]] != 'm'){
-      board[mine[x]-1][mine[y]] = board[mine[x]-1][mine[y]] + 1;
+    if(!mine_left && board[mine['x']-1][mine['y']] != 'm'){
+      board[mine['x']-1][mine['y']] = board[mine['x']-1][mine['y']] + 1;
     }
   }
-  return board
-}
+  return board;
+};
   
 function unique_mine(board, w, h){
-  var x = Math.floor((Math.random() * w) + 1);
-  var y = Math.floor((Math.random() * h) + 1);
-  
+  var x = Math.floor((Math.random() * w));
+  var y = Math.floor((Math.random() * h));
+  var unique;
   if(board[x][y] != 'm'){
-    return {x: x, y: y};
+    unique = {x: x, y: y};
   } else {
-    return unique_mine(board, w, h);
+    unique = unique_mine(board, w, h);
   }
-}
+  return unique;
+};
     
 function blank_board(w, h){
   //returns w by h board filled with 0's meaning 0 mines around this spot
-  var board = []
-  var column = []
-  for(var i =0;i<h;i++) { column.push(0); }
-  for(var i=0;i<w;i++) { board.push(column); }
-  return board
-}
+  var board = Array(w);
+  for(var i=0;i<w;i++) { 
+    board[i] = Array(h);
+    for(var j =0;j<h;j++) { board[i][j] = 0; } 
+  }
+  return board;
+};
 
 function get_board_data_on_page_load(){
 	var starting_bombs = parseInt(document.getElementById("mines").innerHTML);
-	var b_height = document.getElementById(board-table).getElementsByTagName("tr").length;
-	var b_length = document.getElementById(board-table).getElementsByTagName("tr")[0].getElementsByTagName("td").length;
+	var b_height = document.getElementById('board-table').getElementsByTagName("tr").length;
+	var b_length = document.getElementById('board-table').getElementsByTagName("tr")[0].getElementsByTagName("td").length;
 	return {h:b_height,l:b_length,b:starting_bombs};
-}
+};
 
 //Code to initialize the clock. Returns a function that will reset the clock.
 function initialize_clock(){
